d4a01670ba4d92c15315cd73e65cd4cc7a4c7214
XyL1GaN4eG <trolloguy@mail.ru>
2024-07-16 23:49:18 +0300

    delete api service

--- apiService/src/main/java/org/example/tgClient/callbacks/CallbackHandler.java
+++
@@ -1,9 +0,0 @@
-package org.example.tgClient.callbacks;
-
-import org.springframework.cglib.proxy.Callback;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-public interface CallbackHandler {
-    SendMessage apply(Callback callback, Update update);
-}
