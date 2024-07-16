8e8550a1c9dcfea8fdf0b5075d0cca6082add7ac
XyL1GaN4eG <trolloguy@mail.ru>
2024-07-16 23:46:43 +0300

    Delete apiService

--- apiService/src/main/java/org/example/tgClient/commands/Command.java
+++
@@ -1,8 +0,0 @@
-package org.example.tgClient.commands;
-
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-public interface Command {
-    SendMessage apply(Update update);
-}
