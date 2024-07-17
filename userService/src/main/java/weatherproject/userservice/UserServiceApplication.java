6fccdd037435b10c6cd2e3a9672a17acd885f87a
XyL1GaN4eG <trolloguy@mail.ru>
2024-07-17 10:56:51 +0300

    Update application yml and application class

--- userService/src/main/java/weatherproject/userservice/UserServiceApplication.java
+++ userService/src/main/java/weatherproject/userservice/UserServiceApplication.java
@@ -1,9 +1,17 @@
 package weatherproject.userservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
