1ca2f59e14e49ff40b655a2d1156c8f441a56541
XyL1GaN4eG <trolloguy@mail.ru>
2024-07-20 18:06:33 +0300

    Update TemperatureChangeNotifier.java

--- weatherApiService/src/main/java/weatherproject/weatherapiservice/listener/TemperatureChangeNotifier.java
+++ weatherApiService/src/main/java/weatherproject/weatherapiservice/listener/TemperatureChangeNotifier.java
@@ -1,13 +1,12 @@
 package weatherproject.weatherapiservice.listener;
+
 import org.postgresql.PGNotification;
-import org.springframework.amqp.core.AmqpTemplate;
+import org.slf4j.Logger;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import weatherproject.weatherapiservice.config.RabbitMQConfig;
-import weatherproject.weatherapiservice.service.WeatherService;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -20,6 +19,8 @@ import java.sql.Statement;
 @Component
 public class TemperatureChangeNotifier {
     private final RabbitTemplate rabbitTemplate;
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(TemperatureChangeNotifier.class);
 
     // Автоматически подставляемый DataSource, который настроен в конфигурации Spring
     @Autowired
@@ -42,36 +43,37 @@ public class TemperatureChangeNotifier {
     // Метод, который будет выполнен после создания бина (инициализация)
     @PostConstruct
     public void start() {
+        log.info("Начинаем поток для TemperatureChangeNotifier");
         // Создаем и запускаем новый поток для прослушивания уведомлений
         listenerThread = new Thread(() -> {
+            log.info("TemperatureChangeNotifier started");
             try {
-                // Получаем соединение из DataSource
+                log.info("Получаем соединение в бд");
                 connection = dataSource.getConnection();
-                // Создаем SQL-запрос для подписки на уведомления
+                log.info("Создаем SQL-запрос для подписки на уведомления");
                 connection.createStatement().execute("LISTEN my_notification");
 
-                // Бесконечный цикл для постоянного прослушивания
+                log.info("Начинаем бесконечный цикл для постоянного прослушивания");
                 while (!Thread.currentThread().isInterrupted()) {
                     try (Statement stmt = connection.createStatement()) {
-                        // Повторно подписываемся на уведомления (периодически)
+                        log.info("Повторно создаем SQL-запрос для подписки на уведомления");
                         stmt.execute("LISTEN my_notification");
 
                         // Проверяем наличие новых уведомлений с таймаутом в 5000 мс
                         PGNotification[] notificate = connection.unwrap(org.postgresql.PGConnection.class).getNotifications(5000);
 
                         // Обработка уведомлений (тут можно добавить вашу логику)
-                        System.out.println("Notification received" + notificate[0]);
+                        log.info("Получено уведомление от триггера с бд: {}", notificate[0]);
 
                         rabbitTemplate.convertAndSend(notificationQueue.getName(), notificate[0].getParameter());
 
                     } catch (SQLException e) {
                         // Выводим сообщение об ошибке в консоль
-                        e.printStackTrace();
+                        log.error("Произошла ошибка во время прослушивания уведомлений: {}", e.getMessage());
                     }
                 }
             } catch (SQLException e) {
-                // Обрабатываем исключение при установке соединения
-                e.printStackTrace();
+                log.error("Исключение при установке соединения: {}", e.getMessage());
             }
         });
         // Запускаем поток
