c92cdfaad95aa0ffce50ca7537c8ed879ebf888b
XyL1GaN4eG <trolloguy@mail.ru>
2024-07-25 18:29:06 +0300

    Update

--- tgBotService/src/main/java/weatherproject/tgbotservice/config/RabbitMQConfig.java
+++ tgBotService/src/main/java/weatherproject/tgbotservice/config/RabbitMQConfig.java
@@ -1,5 +1,7 @@
 package weatherproject.tgbotservice.config;
 
 public class RabbitMQConfig {
+
+    //TODO: сделать чтобы прослушивалась очередь
     public static final String NOTIFICATION_QUEUE = "weather-notification-queue";
 }
--- tgBotService/src/main/java/weatherproject/tgbotservice/telegram/callbacks/CallbackHandler.java
+++ tgBotService/src/main/java/weatherproject/tgbotservice/telegram/callbacks/CallbackHandler.java
@@ -1,6 +1,7 @@
 package weatherproject.tgbotservice.telegram.callbacks;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -18,6 +19,7 @@ import static weatherproject.tgbotservice.utils.Constants.NEW_CITY_SETTED;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class CallbackHandler {
     private final GeocodingClient geocodingClient;
     private final WeatherServiceClient weatherServiceClient;
@@ -31,6 +33,7 @@ public class CallbackHandler {
         String textToReply = "Просим прощения, город или погода в нем не найдены.";
 
         var currentState = (UserState) UserState.valueOf(currentUser.getState());
+        log.info("Обрабатываем {} от пользователя ", update.getMessage(), currentUser);
         //TODO: вынести обработку сообщения и присваивания названия города в отдельный метод
         //TODO: вынести коллбэки в отдельные классы
         //TODO: делать проверку в сообщении на то, город ли это вообще
@@ -43,20 +46,23 @@ public class CallbackHandler {
                             update.getMessage().getLocation().getLatitude(),
                             update.getMessage().getLocation().getLongitude()));
                 }
+                log.info("Пытаемся получить погоду в городе {}", city);
                 var weatherCity = weatherServiceClient.getWeatherByCity(city);
                 if (weatherCity != null) {
+                    log.info("Получена погода: {}", weatherCity);
                     userServiceClient.createOrUpdateUser(new UserDTO(currentUser.getChatId(), city, HAVE_SETTED_CITY.toString()));
                     textToReply = NEW_CITY_SETTED
                             .replace("{city}", translateClient.translateEngToRussian(weatherCity.getCity()))
                             .replace("{temperature}", weatherCity.getTemperature().toString())
                             .replace("{condition}", translateClient.translateEngToRussian(weatherCity.getCondition()));
+                    log.info("Сообщение для отправки: {}", textToReply);
                 }
                 return new SendMessage(chatId.toString(), textToReply);
             }
 
             //Если город уже выставлен
             case HAVE_SETTED_CITY: {
-                //то обращаемся к апи и возвращаем название города в на английском языке
+                //то обращаемся к апи и возвращаем название города на английском языке
                 if (update.getMessage().hasText()) {
                     city = translateClient.translateRuToEng(update.getMessage().getText()).replace(" ", "-");
                 } else if (update.getMessage().hasLocation()) {
@@ -64,11 +70,12 @@ public class CallbackHandler {
                             update.getMessage().getLocation().getLatitude(),
                             update.getMessage().getLocation().getLongitude());
                 }
+                log.info("Пытаемся получить погоду в городе {}", city);
                 //получаем погоду из апи микросервиса
                 var weatherCity = weatherServiceClient.getWeatherByCity(city);
 
-                //TODO: добавить чтобы если город не найден, то написать об этом в сообщении
                 if (weatherCity != null) { //если погода найдена то
+                    log.info("Получена погода: {}", weatherCity);
                     //обновляем город пользователя
                     userServiceClient.createOrUpdateUser(new UserDTO(currentUser.getChatId(), city, HAVE_SETTED_CITY.toString()));
                     textToReply = NEW_CITY_SETTED
