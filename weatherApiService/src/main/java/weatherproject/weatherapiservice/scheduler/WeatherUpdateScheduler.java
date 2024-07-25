c2884683d53d130ecb75c330b043a8cda9f2bd60
XyL1GaN4eG <trolloguy@mail.ru>
2024-07-25 21:54:52 +0300

    Update WeatherUpdateScheduler.java

--- weatherApiService/src/main/java/weatherproject/weatherapiservice/scheduler/WeatherUpdateScheduler.java
+++ weatherApiService/src/main/java/weatherproject/weatherapiservice/scheduler/WeatherUpdateScheduler.java
@@ -16,7 +16,7 @@ public class WeatherUpdateScheduler {
     }
 
     //Каждый час сохраняет погоду в БД, чтобы если вдруг сработал триггер
-    @Scheduled(cron = "54 * * * *")
+    @Scheduled(cron = "58 * * * * *")
     public void updateWeather() {
         log.info("Ежечасное обновление погоды началось...");
         weatherService.updateAllCitiesWeather();
